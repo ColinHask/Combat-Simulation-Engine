f7754163b0776ffcee820f150739b1c4e221845f
ColinHask <colinhaskins2022@gmail.com>
2025-05-13 23:49:36 -0400

    added location unit helper implemented basic Unit::move

--- src/Unit.cpp
+++ src/Unit.cpp
@@ -12,9 +12,25 @@ std::string Unit::get_team(){
     return team_;
 }
 
+// location helper
+void Unit::update_location(Grid* grid){
+
+    std::pair<int, int> location = grid->find_unit_coords(this);
+
+    if (location == std::make_pair(-1, -1)){
+        std::cout << "Unit does not exist (BAD) (this shouldnt happen) ==============================\n";
+        return;
+    } else {
+        x_ = location.first;
+        y_ = location.second;
+    }
+
+}
+
 // moves unit by continuously attempting to move via grid::try_move
 // note: x and y are not Goal Locations, they are change in x and y
 void Unit::move(Grid* grid){
+
     // find self coords using grid method (and update internal location)
     // determine goal coords
     // loop:
@@ -25,5 +41,33 @@ void Unit::move(Grid* grid){
     //  else:
     //   iterate goal coords (rotate around current location)
     //   count++ (if > 8 break and say move doesnt happen)
+    std::vector<std::pair<int, int>> moves = {
+    {0, 1},  // down
+    {1, 0},  // right
+    {0, -1}, // up
+    {-1, 0}, // left
+    {1, 1},  // diag down-right
+    {-1, 1}, // diag down-left
+    {1, -1}, // diag up-right
+    {-1, -1} // diag up-left
+    };
+
+    std::pair<int, int> location = grid->find_unit_coords(this);
+
+    // find location for future different unit behavior using this info
+    update_location(grid);
+    
+
+    for (const std::pair<int, int>& move : moves){
+        bool moved = grid->try_move(this, move.first, move.second);
+        if (moved){
+            std:: cout << "Unit Moved!\n";
+            update_location(grid);
+            return;
+        }
+    }
+    std::cout << "Unit didnt move, all attempts failed\n";
+
+
 
 }
