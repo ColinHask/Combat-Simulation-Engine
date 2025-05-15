e7296e57c689c3afc89aa67bc557ebfd586632d5
ColinHask <colinhaskins2022@gmail.com>
2025-05-14 22:41:47 -0400

    added to do list in GC.hpp

---
+++ include/GameController.hpp
@@ -0,0 +1,21 @@
+// GameController TO DO:
+// - run sim
+// - check conflict each tick
+// - compute confloct using unit power scores (implement this in unit)
+// - remove units on death
+// - detect win
+// - timeout feature
+// - basically do what main does
+// eventually:
+// - render json inputs -> sim
+// - output json file of stats / results
+
+// Main TO DO:
+// text UI to enter units
+// calls to GameController to start sim
+// contemplate whether
+// current plan: have main input units from text UI, but game controller handles json -> sim
+
+// Quality of life:
+// - remove debugging prints in other classes
+// - possibly replace debug prints with a log file of moves
